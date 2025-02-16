41ba8864422bad8094197b1dbacb0bf58521fd40
Slava <v4slava.guk@gmail.com>
2025-02-16 19:28:01 +0300

    create posts fk for users mugration

--- backend/core/models/post.py
+++ backend/core/models/post.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, String, Integer, DateTime, sql
+from sqlalchemy import Column, String, Integer, sql, ForeignKey, DateTime, sql
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
 
 
-class Post(Base):
+class Post(Base, IntIdPkMixin):
     __tablename__ = "microblog_posts"
 
-    id = Column(Integer, primary_key=True, index=True, unique=True)
+    # id = Column(Integer, primary_key=True, index=True, unique=True)
     title = Column(String)
     text = Column(String(350))
     date = Column(DateTime(timezone=True), server_default=sql.func.now())
+    # user = Column(String, ForeignKey('users.id'))
+    user: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user_id = relationship("User")
