4e41e97e9efbafb21f4bffef5fcdcf243809e3f0
Slava <v4slava.guk@gmail.com>
2025-02-24 18:16:06 +0300

    change access_token_strategy

--- backend/api/dependencies/authentication/backend.py
+++ backend/api/dependencies/authentication/backend.py
@@ -4,7 +4,7 @@ from core.authentication.transport import bearer_transport
 from api.dependencies.authentication.strategy import get_jwt_strategy
 
 authentication_backend = AuthenticationBackend(
-    name="access-tokens-db",
+    name="access-tokens",
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
--- backend/api/dependencies/authentication/strategy.py
+++ backend/api/dependencies/authentication/strategy.py
@@ -1,33 +1,8 @@
-from typing import (
-    TYPE_CHECKING,
-    Annotated,
-)
-from fastapi import Depends
 from fastapi_users.authentication import JWTStrategy
-from fastapi_users.authentication.strategy.db import (
-    AccessTokenDatabase,
-    DatabaseStrategy,
-)
-
-from api.dependencies.authentication.access_tokens import get_access_tokens_db
 from core.config import settings
-
-# if TYPE_CHECKING:
-#     from core.models import AccessToken
-
 
 SECRET = "SECRET"
 
+
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=settings.access_token.lifetime_seconds)
-
-# def get_database_strategy(
-#         access_token_db: Annotated[
-#             AccessTokenDatabase["AccessToken"],
-#             Depends(get_access_tokens_db),
-#         ],
-# ) -> DatabaseStrategy:
-#     return DatabaseStrategy(
-#         database=access_token_db,
-#         lifetime_seconds=settings.access_token.lifetime_seconds,
-#     )
