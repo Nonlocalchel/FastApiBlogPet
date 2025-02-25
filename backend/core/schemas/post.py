dcbd6def7abc6061f264b03d64928d43a912aea2
Slava <v4slava.guk@gmail.com>
2025-02-25 15:10:49 +0300

    change post lists item schema

--- backend/core/schemas/post.py
+++ backend/core/schemas/post.py
@@ -11,23 +11,15 @@ class PostBase(BaseModel):
 
 
 class PostList(PostBase):
-    model_config = ConfigDict(from_attributes=True)
-
     id: Optional[int]
     date: Optional[datetime]
-    user: UserRead | None
-    # user: Optional[UserRead]| None
-
-    # class Config:
-    #     from_attributes = True
-
-
+    user_id: int | None
 
 
 class PostSingle(PostList):
     pass
 
-class PostCreate(PostBase):
 
+class PostCreate(PostBase):
     class Config:
         from_attributes = True
