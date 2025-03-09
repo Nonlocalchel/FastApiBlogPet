c7c0f1645386b95c165b6e885ae3acad66767268
Slava <v4slava.guk@gmail.com>
2025-03-09 14:28:18 +0300

    add post handler test

--- backend/tests/test-handlers/test_posts_hendlers.py
+++ backend/tests/test-handlers/test_posts_hendlers.py
@@ -12,3 +12,14 @@ async def test_get_posts(client: AsyncClient):
 async def test_get_post(client: AsyncClient):
     response = await client.get("/api/v1/posts/1")
     assert response.status_code == 404
+
+
+@pytest.mark.asyncio(loop_scope="function")
+async def test_create_post(client: AsyncClient, auth_token: str):
+    post_data = {
+        "title": "ss",
+        "body": "ss"
+    }
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = await client.post("/api/v1/posts/", json=post_data, headers=headers)
+    assert response.status_code == 201
