f8e5731f672c1010b16a5d0c52f1524d32cc1c23
Slava <v4slava.guk@gmail.com>
2025-03-11 18:15:39 +0300

    docker

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -20,20 +20,16 @@ class Base(DeclarativeBase):
         return f"{camel_case_to_snake_case(cls.__name__)}s"
 
     def transform_model_to_dict(self):
-        # Обработка простых колонок
         data = {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
-        # Обработка отношений (вложенных моделей)
         for relation in self.__mapper__.relationships:
             related_obj = getattr(self, relation.key)
             if related_obj is None:
                 data[relation.key] = None
-            # Если связь возвращает список объектов (one-to-many, many-to-many)
             elif isinstance(related_obj, list):
                 data[relation.key] = [
                     item.transform_model_to_dict() if isinstance(item, Base) else item for item in related_obj
                 ]
-            # Если связь возвращает один объект (one-to-one, many-to-one)
             else:
                 data[relation.key] = (
                     related_obj.transform_model_to_dict() if isinstance(related_obj, Base) else related_obj
