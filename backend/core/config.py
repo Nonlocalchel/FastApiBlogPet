f37c598bf72e05e170eaff70a741bdd3922dcdda
Slava <v4slava.guk@gmail.com>
2025-03-18 14:19:32 +0300

    fix tests

--- backend/core/config.py
+++ backend/core/config.py
@@ -45,11 +45,9 @@ class DatabaseConfig(BaseModel):
     def url(self):
         is_testing = os.getenv("TESTING")
         if is_testing:
-            if self.host != 'localhost':
-                return f'postgresql+asyncpg://{self.user}_test:{self.password}_test@{self.host}_test:{self.port}/{self.name}_test'
+            return f'postgresql+asyncpg://{self.user}_test:{self.password}_test@{self.host}:{int(self.port)+1}/{self.name}_test'
 
-            return f'postgresql+asyncpg://{self.user}_test:{self.password}_test@{self.host}:5433/{self.name}_test'
-
+        print(f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}')
         return f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}'
 
     naming_convention: dict[str, str] = {
